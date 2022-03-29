5b8cd02dc8af2770dc47efaaec46d1fa85577e68
ArtistStreet <darkstarcfvn@gmail.com>
2022-03-29 12:02:30 +0700

    1

--- js.js
+++ js.js
@@ -70,3 +70,72 @@ const handleButtonClick = (e) => {
 buttons.forEach((btn) => {
   btn.addEventListener("click", handleButtonClick);
 });
+var message = "what do you want? ctr shift c or ctr u for sure";
+
+function rtclickcheck(keyp) {
+  if (navigator.appName == "Netscape" && keyp.which == 3) {
+    alert(message);
+    return false;
+  }
+
+  if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) {
+    alert(message);
+    return false;
+  }
+}
+
+document.onmousedown = rtclickcheck;
+function mousehandler(e) {
+  var myevent = isNS ? e : event;
+  var eventbutton = isNS ? myevent.which : myevent.button;
+  if (eventbutton == 2 || eventbutton == 3) return false;
+}
+
+// document.oncontextmenu = mischandler;
+// document.onmousedown = mousehandler;
+// document.onmouseup = mousehandler;
+// function disableCtrlKeyCombination(e) {
+//   var forbiddenKeys = new Array("a", "s", "c", "x", "u");
+//   var key;
+//   var isCtrl;
+//   if (window.event) {
+//     key = window.event.keyCode;
+//     //IE
+//     if (window.event.ctrlKey) isCtrl = true;
+//     else isCtrl = false;
+//   } else {
+//     key = e.which;
+//     //firefox
+//     if (e.ctrlKey) isCtrl = true;
+//     else isCtrl = false;
+//   }
+//   if (isCtrl) {
+//     for (i = 0; i < forbiddenKeys.length; i++) {
+//       //case-insensitive comparation
+//       if (
+//         forbiddenKeys[i].toLowerCase() == String.fromCharCode(key).toLowerCase()
+//       ) {
+//         return false;
+//       }
+//     }
+//   }
+//   return true;
+// }
+// function unloadJS(scriptName) {
+//   var head = document.getElementsByTagName("head").item(0);
+//   var js = document.getElementById(scriptName);
+//   js.parentNode.removeChild(js);
+// }
+
+// //----------------------
+// function unloadAllJS() {
+//   var jsArray = new Array();
+//   jsArray = document.getElementsByTagName("script");
+//   for (i = 0; i < jsArray.length; i++) {
+//     if (jsArray[i].id) {
+//       unloadJS(jsArray[i].id);
+//     } else {
+//       jsArray[i].parentNode.removeChild(jsArray[i]);
+//     }
+//   }
+// }
