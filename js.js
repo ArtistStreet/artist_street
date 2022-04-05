e70a5587276e7cf29e7e6cb4fa57a464d7f7dfb3
ArtistStreet <darkstarcfvn@gmail.com>
2022-04-05 20:08:24 +0700

    1

--- js.js
+++ js.js
@@ -151,8 +151,8 @@ window.onload = function () {
     body.style.background = "url(https://wallpaperaccess.com/full/670779.jpg)";
   }, 15000);
 };
-document.addEventListener("keydown", function (event) {
-  if (event.ctrlKey) {
-    event.preventDefault();
-  }
-});
+// document.addEventListener("keydown", function (event) {
+//   if (event.ctrlKey) {
+//     event.preventDefault();
+//   }
+// });
